ed1eecc8183a65611be0eb02b32d699ed3daa6f3
abhinuvpitale <abhinuvpitale@gmail.com>
2017-03-28 22:29:12 +0530

    Create verge_newsfeed_spider.py

---
+++ verge_newsfeed_spider.py
@@ -0,0 +1,19 @@
+import urllib
+from bs4 import BeautifulSoup
+from sys import argv
+import bs4
+import unicodedata
+import re
+
+url_base = "https://www.theverge.com"
+html = urllib.urlopen(''+url_base).read()
+soup = BeautifulSoup(html,'html.parser')
+tags_span = soup('a')
+target = open('test.docx','w')
+
+for item in tags_span:
+	text = item.get('data-analytics-link')
+	if 'article' == text:
+		target.write('---------------------------------------------------------------------------------------\n')
+		target.write(str(item.text.encode('utf-8')))
+		target.write('\n')
