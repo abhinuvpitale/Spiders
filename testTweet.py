5c72ebae6f6aaee2778c248ddf1226b67b940b3a
abhinuvpitale <abhinuvpitale@users.noreply.github.com>
2017-04-29 14:58:30 +0530

    Update testTweet.py

--- testTweet.py
+++ testTweet.py
@@ -1,6 +1,6 @@
 from twitter import Twitter,OAuth
 
-%OAuth Details replace ID1,ID2,ID3,ID4
+#OAuth Details replace ID1,ID2,ID3,ID4
 tw = Twitter(auth = OAuth(ID1,ID2,ID3,ID4))
 print tw
 tw.statuses.update(status = "Hello World!!")
